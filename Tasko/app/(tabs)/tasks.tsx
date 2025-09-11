8a0bab5e6ebf4492a91878258b4ebe2f5e2d402f
Mathis Baveye <mathisbaveye@MacBook-Air-de-Mathis.local>
2025-09-11 16:32:41 +0200

    use env

--- Tasko/app/(tabs)/tasks.tsx
+++ Tasko/app/(tabs)/tasks.tsx
@@ -1,158 +1,226 @@
-import {ThemedText} from "@/components/ThemedText";
-import {Pressable, SafeAreaView, StyleSheet, View} from "react-native";
-import {SafeAreaProvider} from "react-native-safe-area-context";
-import {FlashList} from "@shopify/flash-list";
+import { ThemedText } from "@/components/ThemedText";
+import { Pressable, SafeAreaView, StyleSheet, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { FlashList } from "@shopify/flash-list";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import React from "react";
-import {MaterialCommunityIcons} from "@expo/vector-icons";
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
-import {Link} from "expo-router";
-import {Checkbox} from "expo-checkbox";
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
-import { useFocusEffect } from '@react-navigation/native';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import { Link } from "expo-router";
+import { Checkbox } from "expo-checkbox";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useFocusEffect } from "@react-navigation/native";
+const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 
 export default function TasksScreen() {
-    const [tasks, setTasks] = React.useState<any[]>([]);
-    const insets = useSafeAreaInsets();
-    useFocusEffect(() => {
-        fetch("http://10.112.132.249:1337/api/tasks", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                const filteredTasks = data.data.filter((task: any) => task.taskStatus !== "done");
-                setTasks(filteredTasks);
-            })
-            .catch((error) => {
-                console.error("Error fetching tasks:", error);
-            });
-    });
+  const [tasks, setTasks] = React.useState<any[]>([]);
+  const insets = useSafeAreaInsets();
+  useFocusEffect(() => {
+    fetch(`${apiUrl}/api/tasks`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const filteredTasks = data.data.filter(
+          (task: any) => task.taskStatus !== "done"
+        );
+        setTasks(filteredTasks);
+      })
+      .catch((error) => {
+        console.error("Error fetching tasks:", error);
+      });
+  });
 
-    const setChecked = async (checkedItem: any) => {
-        checkedItem.taskStatus = "done";
-        try {
-            await fetch(`http://10.112.132.249:1337/api/tasks/${checkedItem.documentId}`, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    data: { taskStatus: checkedItem.taskStatus }
-                }),
-            });
-            setTasks(prevTasks =>
-                prevTasks.map(task =>
-                    task.id === checkedItem.id
-                        ? { ...task, taskStatus: checkedItem.taskStatus }
-                        : task
-                )
-            );
-        } catch (error) {
-            console.error("Erreur lors de la mise à jour du statut :", error);
+  const setChecked = async (checkedItem: any) => {
+    checkedItem.taskStatus = "done";
+    try {
+      await fetch(`${apiUrl}/api/tasks/${checkedItem.documentId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          data: { taskStatus: checkedItem.taskStatus },
+        }),
+      });
+      setTasks((prevTasks) =>
+        prevTasks.map((task) =>
+          task.id === checkedItem.id
+            ? { ...task, taskStatus: checkedItem.taskStatus }
+            : task
+        )
+      );
+    } catch (error) {
+      console.error("Erreur lors de la mise à jour du statut :", error);
+    }
+  };
+
+  const renderFlashListItems = ({ item }: { item: any }) => (
+    <View style={styles.itemContainer}>
+      {getIconFromCategory(item.taskCategory)}
+      <View>
+        <ThemedText
+          darkColor={"white"}
+          lightColor={"white"}
+          style={
+            item.taskStatus === "done"
+              ? { textDecorationLine: "line-through" }
+              : {}
+          }
+        >
+          {item.taskName}
+        </ThemedText>
+        <ThemedText
+          darkColor={"#ada692"}
+          lightColor={"#ada692"}
+          style={
+            item.taskStatus === "done"
+              ? { textDecorationLine: "line-through" }
+              : {}
+          }
+        >
+          {new Date(item.taskDate).toLocaleString("fr-FR", {
+            dateStyle: "medium",
+            timeStyle: "short",
+          })}
+        </ThemedText>
+      </View>
+      <Checkbox
+        value={item.taskStatus === "done"}
+        onValueChange={
+          item.taskStatus === "done" ? undefined : () => setChecked(item)
         }
-    };
+        color={item.taskStatus === "done" ? "#ed8c37" : undefined}
+      />
+    </View>
+  );
 
-    const renderFlashListItems = ({item}: { item: any }) => (
-        <View style={styles.itemContainer}>
-            {getIconFromCategory(item.taskCategory)}
-            <View>
-                <ThemedText darkColor={"white"} lightColor={"white"}
-                            style={item.taskStatus === "done" ? {textDecorationLine: "line-through"} : {}}>
-                    {item.taskName}
-                </ThemedText>
-                <ThemedText darkColor={"#ada692"} lightColor={"#ada692"}
-                            style={item.taskStatus === "done" ? {textDecorationLine: "line-through"} : {}}>
-                    {new Date(item.taskDate).toLocaleString('fr-FR', {dateStyle: 'medium', timeStyle: 'short'})}
-                </ThemedText>
-            </View>
-            <Checkbox
-                value={item.taskStatus === "done"}
-                onValueChange={item.taskStatus === 'done' ? undefined : () => setChecked(item)}
-                color={item.taskStatus === "done" ? "#ed8c37" : undefined}
-            />
-        </View>
-    );
-
-    return (
-        <View style={{
-            flex: 1,
-            paddingTop: insets.top + 16,
-            paddingBottom: insets.bottom,
-            paddingLeft: insets.left,
-            paddingRight: insets.right,
-            backgroundColor: "#1B1611",
-            justifyContent: "flex-start",
-            flexDirection: "column"
-        }}>
-            <View style={styles.titleContainer}>
-                <View style={{width: 28}}/>
-                <ThemedText darkColor={"white"} lightColor={"white"} type={"title"}>Tâches</ThemedText>
-                <Link href="/addTask" asChild>
-                    <Pressable>
-                        <MaterialIcons name="add" size={28} color="white"/>
-                    </Pressable>
-                </Link>
-            </View>
-            <FlashList
-                renderItem={renderFlashListItems}
-                data={tasks}
-                keyExtractor={item => item.id?.toString()}
-                ItemSeparatorComponent={() => <View style={{height: 8}}/>}
-                estimatedItemSize={50}
-            />
-        </View>
-    );
+  return (
+    <View
+      style={{
+        flex: 1,
+        paddingTop: insets.top + 16,
+        paddingBottom: insets.bottom,
+        paddingLeft: insets.left,
+        paddingRight: insets.right,
+        backgroundColor: "#1B1611",
+        justifyContent: "flex-start",
+        flexDirection: "column",
+      }}
+    >
+      <View style={styles.titleContainer}>
+        <View style={{ width: 28 }} />
+        <ThemedText darkColor={"white"} lightColor={"white"} type={"title"}>
+          Tâches
+        </ThemedText>
+        <Link href="/addTask" asChild>
+          <Pressable>
+            <MaterialIcons name="add" size={28} color="white" />
+          </Pressable>
+        </Link>
+      </View>
+      <FlashList
+        renderItem={renderFlashListItems}
+        data={tasks}
+        keyExtractor={(item) => item.id?.toString()}
+        ItemSeparatorComponent={() => <View style={{ height: 8 }} />}
+        estimatedItemSize={50}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    titleContainer: {
-        flexDirection: 'row',
-        alignItems: "center",
-        justifyContent: "space-between",
-        marginBottom: 32,
-        marginHorizontal: 16,
-    },
-    itemContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-around",
-        borderBottomWidth: 1,
-        borderBottomColor: "#ccc",
-        backgroundColor: "#3d2d1d",
-        borderRadius: 8,
-        borderColor: "#3d2d1d",
-        paddingVertical: 8,
-        marginHorizontal: 8,
-    },
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 32,
+    marginHorizontal: 16,
+  },
+  itemContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    backgroundColor: "#3d2d1d",
+    borderRadius: 8,
+    borderColor: "#3d2d1d",
+    paddingVertical: 8,
+    marginHorizontal: 8,
+  },
 });
 
 const getIconFromCategory = (category: string) => {
-    switch (category) {
-        case "Cleaning":
-            return <MaterialIcons style={{backgroundColor: "#724621", borderRadius: 8, padding: 8}}
-                                  name="cleaning-services" size={24} color="#ed8c37"/>
-        case "Tidying":
-            return <MaterialCommunityIcons style={{backgroundColor: "#724621FF", borderRadius: 8, padding: 8}}
-                                           name="broom" size={24} color="#ed8c37"/>;
-        case "Laundry":
-            return <MaterialCommunityIcons style={{backgroundColor: "#724621FF", borderRadius: 8, padding: 8}}
-                                           name="tshirt-crew" size={24} color="#ed8c37"/>;
-        case "Cooking":
-            return <MaterialCommunityIcons style={{backgroundColor: "#724621FF", borderRadius: 8, padding: 8}}
-                                           name="chef-hat" size={24} color="#ed8c37"/>;
-        case "Shopping":
-            return <FontAwesome6 style={{backgroundColor: "#724621FF", borderRadius: 8, padding: 8}} name="sack-dollar"
-                                 size={24} color="#ed8c37"/>;
-        case "Housekeeping":
-            return <MaterialCommunityIcons style={{backgroundColor: "#724621FF", borderRadius: 8, padding: 8}}
-                                           name="home-heart" size={24} color="#ed8c37"/>;
-        case "Gardening":
-            return <MaterialCommunityIcons style={{backgroundColor: "#724621FF", borderRadius: 8, padding: 8}}
-                                           name="flower-tulip" size={24} color="#ed8c37"/>;
-        default:
-            return null;
-    }
+  switch (category) {
+    case "Cleaning":
+      return (
+        <MaterialIcons
+          style={{ backgroundColor: "#724621", borderRadius: 8, padding: 8 }}
+          name="cleaning-services"
+          size={24}
+          color="#ed8c37"
+        />
+      );
+    case "Tidying":
+      return (
+        <MaterialCommunityIcons
+          style={{ backgroundColor: "#724621FF", borderRadius: 8, padding: 8 }}
+          name="broom"
+          size={24}
+          color="#ed8c37"
+        />
+      );
+    case "Laundry":
+      return (
+        <MaterialCommunityIcons
+          style={{ backgroundColor: "#724621FF", borderRadius: 8, padding: 8 }}
+          name="tshirt-crew"
+          size={24}
+          color="#ed8c37"
+        />
+      );
+    case "Cooking":
+      return (
+        <MaterialCommunityIcons
+          style={{ backgroundColor: "#724621FF", borderRadius: 8, padding: 8 }}
+          name="chef-hat"
+          size={24}
+          color="#ed8c37"
+        />
+      );
+    case "Shopping":
+      return (
+        <FontAwesome6
+          style={{ backgroundColor: "#724621FF", borderRadius: 8, padding: 8 }}
+          name="sack-dollar"
+          size={24}
+          color="#ed8c37"
+        />
+      );
+    case "Housekeeping":
+      return (
+        <MaterialCommunityIcons
+          style={{ backgroundColor: "#724621FF", borderRadius: 8, padding: 8 }}
+          name="home-heart"
+          size={24}
+          color="#ed8c37"
+        />
+      );
+    case "Gardening":
+      return (
+        <MaterialCommunityIcons
+          style={{ backgroundColor: "#724621FF", borderRadius: 8, padding: 8 }}
+          name="flower-tulip"
+          size={24}
+          color="#ed8c37"
+        />
+      );
+    default:
+      return null;
+  }
 };
--- Tasko/app/addTask.tsx
+++ Tasko/app/addTask.tsx
@@ -1,171 +1,211 @@
-import React, { useState } from 'react';
-import {ScrollView, TextInput, StyleSheet, Pressable, Platform, View } from 'react-native';
-import { useRouter } from 'expo-router';
-import { ThemedText } from '@/components/ThemedText';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker';
-import { Picker } from '@react-native-picker/picker';
+import React, { useState } from "react";
+import {
+  ScrollView,
+  TextInput,
+  StyleSheet,
+  Pressable,
+  Platform,
+  View,
+} from "react-native";
+import { useRouter } from "expo-router";
+import { ThemedText } from "@/components/ThemedText";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import DateTimePicker, {
+  DateTimePickerEvent,
+} from "@react-native-community/datetimepicker";
+import { Picker } from "@react-native-picker/picker";
 
 export default function AddTaskScreen() {
-    const [taskName, setTaskName] = useState('');
-    const [taskDifficulty, setTaskDifficulty] = useState('easy');
-    const [taskCategory, setTaskCategory] = useState('Cleaning');
-    const [date, setDate] = useState(new Date());
-    const [showPicker, setShowPicker] = useState(false);
-    const [pickerMode, setPickerMode] = useState<'date' | 'time'>('date');
-    const router = useRouter();
-    const insets = useSafeAreaInsets();
+  const [taskName, setTaskName] = useState("");
+  const [taskDifficulty, setTaskDifficulty] = useState("easy");
+  const [taskCategory, setTaskCategory] = useState("Cleaning");
+  const [date, setDate] = useState(new Date());
+  const [showPicker, setShowPicker] = useState(false);
+  const [pickerMode, setPickerMode] = useState<"date" | "time">("date");
+  const router = useRouter();
+  const insets = useSafeAreaInsets();
 
-    const handleAddTask = async () => {
-        if (!taskName.trim() || !taskCategory.trim()) {
-            alert('Veuillez remplir tous les champs.');
-            return;
+  const handleAddTask = async () => {
+    if (!taskName.trim() || !taskCategory.trim()) {
+      alert("Veuillez remplir tous les champs.");
+      return;
+    }
+
+    try {
+      let data = {
+        taskName: taskName.trim(),
+        taskCategory: taskCategory.trim(),
+        taskDifficulty: taskDifficulty.trim(),
+        taskDate: date.toISOString(),
+        taskStatus: "todo",
+      };
+      console.log(data);
+      const response = await fetch(
+        "https://36e2460ec91c.ngrok-free.app/api/tasks",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            data: data,
+          }),
         }
+      );
 
-        try {
-            let data = {
-                taskName: taskName.trim(),
-                taskCategory: taskCategory.trim(),
-                taskDifficulty: taskDifficulty.trim(),
-                taskDate: date.toISOString(),
-                taskStatus: 'todo',
-            };
-            console.log(data)
-            const response = await fetch('http://10.112.132.249:1337/api/tasks', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    data: data,
-                }),
-            });
+      if (response.ok) {
+        router.back();
+      } else {
+        const errorData = await response.json();
+        console.error("Failed to add task:", errorData);
+        alert("Échec de l'ajout de la tâche.");
+      }
+    } catch (error) {
+      console.error("Error adding task:", error);
+      alert("Une erreur est survenue.");
+    }
+  };
 
-            if (response.ok) {
-                router.back();
-            } else {
-                const errorData = await response.json();
-                console.error('Failed to add task:', errorData);
-                alert("Échec de l'ajout de la tâche.");
-            }
-        } catch (error) {
-            console.error('Error adding task:', error);
-            alert('Une erreur est survenue.');
+  const onDateChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
+    const currentDate = selectedDate || date;
+    if (Platform.OS === "android") {
+      setShowPicker(false);
+      if (event.type === "set") {
+        setDate(currentDate);
+        if (pickerMode === "date") {
+          setPickerMode("time");
+          setShowPicker(true); // Re-open for time
         }
-    };
+      }
+    } else {
+      // For iOS
+      setDate(currentDate);
+    }
+  };
 
-    const onDateChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
-        const currentDate = selectedDate || date;
-        if (Platform.OS === 'android') {
-            setShowPicker(false);
-            if (event.type === 'set') {
-                setDate(currentDate);
-                if (pickerMode === 'date') {
-                    setPickerMode('time');
-                    setShowPicker(true); // Re-open for time
-                }
-            }
-        } else { // For iOS
-            setDate(currentDate);
-        }
-    };
+  const showDatepicker = () => {
+    setPickerMode("date"); // Always start with date picker
+    setShowPicker(true);
+  };
 
-    const showDatepicker = () => {
-        setPickerMode('date'); // Always start with date picker
-        setShowPicker(true);
-    };
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          paddingTop: insets.top + 16,
+          paddingBottom: insets.bottom,
+          paddingLeft: insets.left,
+          paddingRight: insets.right,
+        },
+      ]}
+    >
+      <ScrollView>
+        <ThemedText type="title" style={styles.title}>
+          Nouvelle Tâche
+        </ThemedText>
+        <TextInput
+          style={styles.input}
+          placeholder="Nom de la tâche"
+          placeholderTextColor="#888"
+          value={taskName}
+          onChangeText={setTaskName}
+        />
+        <Pressable onPress={showDatepicker} style={styles.input}>
+          <ThemedText style={{ color: "white" }}>
+            {date.toLocaleString("fr-FR", {
+              dateStyle: "medium",
+              timeStyle: "short",
+            })}
+          </ThemedText>
+        </Pressable>
 
-    return (
-        <View style={[styles.container, { paddingTop: insets.top + 16, paddingBottom: insets.bottom, paddingLeft: insets.left, paddingRight: insets.right }]}>
-            <ScrollView>
-                <ThemedText type="title" style={styles.title}>Nouvelle Tâche</ThemedText>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Nom de la tâche"
-                    placeholderTextColor="#888"
-                    value={taskName}
-                    onChangeText={setTaskName}
-                />
-                <Pressable onPress={showDatepicker} style={styles.input}>
-                    <ThemedText style={{ color: 'white' }}>
-                        {date.toLocaleString('fr-FR', { dateStyle: 'medium', timeStyle: 'short' })}
-                    </ThemedText>
-                </Pressable>
+        {showPicker && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode={pickerMode}
+            display="default"
+            onChange={onDateChange}
+            is24Hour={true}
+          />
+        )}
 
-                {showPicker && (
-                    <DateTimePicker
-                        testID="dateTimePicker"
-                        value={date}
-                        mode={pickerMode}
-                        display="default"
-                        onChange={onDateChange}
-                        is24Hour={true}
-                    />
-                )}
+        <Picker
+          selectedValue={taskCategory}
+          style={{
+            width: "100%",
+            backgroundColor: "#3d2d1d",
+            color: "white",
+            marginBottom: 16,
+            borderRadius: 8,
+          }}
+          onValueChange={(itemValue) => setTaskDifficulty(itemValue)}
+          placeholder={"Sélectionner la difficulté..."}
+        >
+          <Picker.Item label="Facile" value="easy" />
+          <Picker.Item label="Moyen" value="medium" />
+          <Picker.Item label="Difficile" value="complex" />
+        </Picker>
 
-                <Picker
-                    selectedValue={taskCategory}
-                    style={{ width: '100%', backgroundColor: '#3d2d1d', color: 'white', marginBottom: 16, borderRadius: 8 }}
-                    onValueChange={(itemValue) => setTaskDifficulty(itemValue)}
-                    placeholder={"Sélectionner la difficulté..."}
-                >
-                    <Picker.Item label="Facile" value="easy" />
-                    <Picker.Item label="Moyen" value="medium" />
-                    <Picker.Item label="Difficile" value="complex" />
-                </Picker>
-
-                <Picker
-                    selectedValue={taskCategory}
-                    style={{ width: '100%', backgroundColor: '#3d2d1d', color: 'white', marginBottom: 16, borderRadius: 8 }}
-                    onValueChange={(itemValue) => setTaskCategory(itemValue)}
-                    placeholder={"Sélectionner la difficulté..."}
-                >
-                    <Picker.Item label="Nettoyage" value="Cleaning" />
-                    <Picker.Item label="Rangement" value="Tidying" />
-                    <Picker.Item label="Lessive" value="Laundry" />
-                    <Picker.Item label="Cuisine" value="Cooking" />
-                    <Picker.Item label="Courses" value="Shopping" />
-                    <Picker.Item label="Entretien de la maison" value="Housekeeping" />
-                    <Picker.Item label="Jardinage" value="Gardening" />
-                </Picker>
-            </ScrollView>
-            <Pressable style={styles.button} onPress={handleAddTask}>
-                <ThemedText style={styles.buttonText}>Ajouter</ThemedText>
-            </Pressable>
-        </View>
-    );
+        <Picker
+          selectedValue={taskCategory}
+          style={{
+            width: "100%",
+            backgroundColor: "#3d2d1d",
+            color: "white",
+            marginBottom: 16,
+            borderRadius: 8,
+          }}
+          onValueChange={(itemValue) => setTaskCategory(itemValue)}
+          placeholder={"Sélectionner la difficulté..."}
+        >
+          <Picker.Item label="Nettoyage" value="Cleaning" />
+          <Picker.Item label="Rangement" value="Tidying" />
+          <Picker.Item label="Lessive" value="Laundry" />
+          <Picker.Item label="Cuisine" value="Cooking" />
+          <Picker.Item label="Courses" value="Shopping" />
+          <Picker.Item label="Entretien de la maison" value="Housekeeping" />
+          <Picker.Item label="Jardinage" value="Gardening" />
+        </Picker>
+      </ScrollView>
+      <Pressable style={styles.button} onPress={handleAddTask}>
+        <ThemedText style={styles.buttonText}>Ajouter</ThemedText>
+      </Pressable>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#1B1611',
-        paddingHorizontal: 16,
-    },
-    title: {
-        marginBottom: 32,
-        textAlign: 'center',
-    },
-    input: {
-        backgroundColor: '#3d2d1d',
-        color: 'white',
-        paddingHorizontal: 16,
-        paddingVertical: 12,
-        borderRadius: 8,
-        marginBottom: 16,
-        fontSize: 16,
-        justifyContent: 'center',
-    },
-    button: {
-        backgroundColor: '#ed8c37',
-        padding: 16,
-        borderRadius: 8,
-        alignItems: 'center',
-        marginTop: 16,
-    },
-    buttonText: {
-        color: '#1B1611',
-        fontWeight: 'bold',
-        fontSize: 16,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#1B1611",
+    paddingHorizontal: 16,
+  },
+  title: {
+    marginBottom: 32,
+    textAlign: "center",
+  },
+  input: {
+    backgroundColor: "#3d2d1d",
+    color: "white",
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    borderRadius: 8,
+    marginBottom: 16,
+    fontSize: 16,
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "#ed8c37",
+    padding: 16,
+    borderRadius: 8,
+    alignItems: "center",
+    marginTop: 16,
+  },
+  buttonText: {
+    color: "#1B1611",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
 });
